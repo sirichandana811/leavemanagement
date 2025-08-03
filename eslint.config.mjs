206f6d583cdafc864e4b1613f478e47c8d705732
sirichandana811 <sirireddy08113@gmail.com>
2025-08-03 21:42:10 +0530

    Fix: Updated NextAuth route exports, ESLint issues, and API page structure

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import eslintPlugin from 'eslint-plugin-next';
+import {eslintPlugin} from 'eslint-plugin-next';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
